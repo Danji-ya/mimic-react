33f1ce3f6b0b72b2cfaca5301f1429bc8e9b4fb5
Danji-ya <erangerang15@gmail.com>
2022-05-24 11:15:18 +0900

    [chore]: 예시코드 수정

--- v3/index.tsx
+++ v3/index.tsx
@@ -8,73 +8,7 @@ interface IBook {
   content: string;
 }
 
-// class Test extends Dj.Component {
-//   constructor(props: AttributeType){
-//     super(props);
-
-//     this.state = {
-//       books: [
-//         { id: 1, completed: false, content: 'star' },
-//         { id: 2, completed: true, content: 'rain' },
-//       ]
-//     }
-
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-
-//   handleClick(event: MouseEvent) {
-//     console.log('상태 변화 그리고 리렌더링 최적화 필요');
-
-//     this.setState({
-//       ...this.state,
-//       books: [
-//         { id: 1, completed: true, content: 'star' },
-//         { id: 2, completed: true, content: 'rain' },
-//         { id: 3, completed: false, content: 'you Dont know JS' },
-//       ]
-//     })
-//   }
-
-//   render() {
-//     const {books} = this.state;
-
-//     return (
-//       <div>
-//         <p>이것은 컴포넌트 리렌더링을 위한 것</p>
-//         <Todo books={books} handleClick={this.handleClick} />
-//       </div>
-//     )
-//   }
-// }
-
-// class Todo extends Dj.Component {
-//   constructor(props: AttributeType){
-//     super(props);
-//   }
-
-//   render() {
-//     const {books} = this.props;
-
-//     return (
-//       <div class="my-component">
-//         {books.length < 3 && <button onClick={this.props.handleClick}>책목록</button>}
-//         <ul>
-//           {books.map((book: IBook) => (
-//             <li>
-//               <input type="checkbox" class="toggle" checked={book.completed} />
-//               {book.content}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     )
-//   }
-// }
-
-// Dj.render(<Test />, document.querySelector('#root'));
-
-class Test extends Dj.Component {
+class App extends Dj.Component {
   constructor(props: AttributeType){
     super(props);
 
@@ -107,8 +41,35 @@ class Test extends Dj.Component {
 
     return (
       <div>
-        <button onClick={this.handleClick}>책목록</button>
-        {/* {books.length < 3 && <button onClick={this.handleClick}>책목록</button>} */}
+        <p>이것은 컴포넌트 리렌더링을 위한 것</p>
+        <div>
+          <Todo books={books} handleClick={this.handleClick} />
+        </div>
+      </div>
+    )
+  }
+}
+
+class Todo extends Dj.Component {
+  constructor(props: AttributeType){
+    super(props);
+    this.handleInput = this.handleInput.bind(this);
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleInput(event: any){
+      this.setState({value: event.target.value})
+  }
+
+  render() {
+    const {books} = this.props;
+
+    return (
+      <div class="my-component">
+        <input type='text' onInput={this.handleInput} value={this.state.value} />
+        <button onClick={this.props.handleClick}>책목록</button>
         <ul>
           {books.map((book: IBook) => (
             <li>
@@ -122,4 +83,50 @@ class Test extends Dj.Component {
   }
 }
 
-Dj.render(<Test />, document.querySelector('#root'));
+Dj.render(<App />, document.querySelector('#root'));
+
+// class App extends Dj.Component {
+//   constructor(props: AttributeType){
+//     super(props);
+
+//     this.state = {
+//       books: [
+//         { id: 1, completed: false, content: 'star' },
+//         { id: 2, completed: true, content: 'rain' },
+//       ]
+//     }
+
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick(event: MouseEvent) {
+//     this.setState({
+//       ...this.state,
+//       books: [
+//         { id: 1, completed: true, content: 'star' },
+//         { id: 2, completed: true, content: 'rain' },
+//         { id: 3, completed: false, content: 'you Dont know JS' },
+//       ]
+//     })
+//   }
+
+//   render() {
+//     const {books} = this.state;
+
+//     return (
+//       <div>
+//         <button onClick={this.handleClick}>{books.length < 3 ? '책추가' : '책정지' }</button>
+//         <ul>
+//           {books.map((book: IBook) => (
+//             <li>
+//               <input type="checkbox" class="toggle" checked={book.completed} />
+//               {book.content}
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+// Dj.render(<App />, document.querySelector('#root'));
