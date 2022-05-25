058d5382bcc0e67a4e8a28be9b7c5e9f9dc4ce1f
Danji-ya <erangerang15@gmail.com>
2022-05-25 14:47:41 +0900

    [chore]: 예제코드 todo로 수정

--- v3/index.tsx
+++ v3/index.tsx
@@ -2,6 +2,8 @@ import Dj from './Dj';
 import { AttributeType } from './types/jsx';
 import './index.css';
 
+let DjIdx = 3;
+
 interface IBook {
   id: number;
   completed: boolean;
@@ -15,6 +17,7 @@ class Todo extends Dj.Component {
       value: ''
     }
     this.handleInput = this.handleInput.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleInput(event: Event){
@@ -25,18 +28,27 @@ class Todo extends Dj.Component {
     })
   }
 
+  handleClick(event: Event){
+    const { value } = this.state;
+    
+    if(value === '') return alert('책이름을 입력해주세요.');
+
+    this.props.addItem(value);
+  }
+  
   render() {
     const {books} = this.props;
 
     return (
       <div class="my-component">
         <input type='text' onInput={this.handleInput} value={this.state.value} />
-        <button onClick={this.props.handleClick}>책추가</button>
+        <button onClick={this.handleClick}>책추가</button>
         <ul>
           {books.map((book: IBook) => (
             <li>
-              <input type="checkbox" class="toggle" checked={book.completed} />
+              <input type="checkbox" class="toggle" checked={book.completed} onChange={() => this.props.checkItem(book.id)} />
               {book.content}
+              <button onClick={() => this.props.removeItem(book.id)}>삭제</button>
             </li>
           ))}
         </ul>
@@ -54,79 +66,54 @@ class App extends Dj.Component {
         { id: 2, completed: true, content: 'rain' },
       ]
     }
-    this.handleClick = this.handleClick.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.checkItem = this.checkItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
   }
 
-  handleClick(event: MouseEvent) {
-    console.log('상태 변화 그리고 리렌더링 최적화 필요');
+  addItem(item: string) {
+    const newBooks = [...this.state.books, {
+      id: DjIdx++,
+      completed: false,
+      content: `newBook:${DjIdx}-${item}`
+    }]
 
     this.setState({
       ...this.state,
-      books: [
-        { id: 1, completed: true, content: 'star' },
-        { id: 2, completed: true, content: 'rain' },
-        { id: 3, completed: false, content: 'you Dont know JS' },
-      ]
-    })
+      books: newBooks,
+    });
+  }
+
+  checkItem(id: number) {
+    const newBooks = this.state.books.map((book: IBook) => {
+      if(book.id === id) book.completed = !book.completed;
+      return book;
+    });
+
+    this.setState({
+      ...this.state,
+      books: newBooks,
+    });
+  }
+
+  removeItem(id: number){
+    const newBooks = this.state.books.filter((book: IBook) => book.id !== id);
+
+    this.setState({
+      ...this.state,
+      books: newBooks,
+    });
   }
 
   render() {
     const {books} = this.state;
+
     return (
       <div>
-        <p>이것은 컴포넌트 리렌더링을 위한 것</p>
-        <div>
-          <Todo books={books} handleClick={this.handleClick} />
-        </div>
+        <Todo books={books} addItem={this.addItem} checkItem={this.checkItem} removeItem={this.removeItem} />
       </div>
     )
   }
 }
 
 Dj.render(<App />, document.querySelector('#root'));
-
-// class App extends Dj.Component {
-//   constructor(props: AttributeType){
-//     super(props);
-
-//     this.state = {
-//       books: [
-//         { id: 1, completed: false, content: 'star' },
-//         { id: 2, completed: true, content: 'rain' },
-//       ]
-//     }
-
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick(event: MouseEvent) {
-//     this.setState({
-//       ...this.state,
-//       books: [
-//         { id: 1, completed: true, content: 'star' },
-//         { id: 2, completed: true, content: 'rain' },
-//         { id: 3, completed: false, content: 'you Dont know JS' },
-//       ]
-//     })
-//   }
-
-//   render() {
-//     const {books} = this.state;
-
-//     return (
-//       <div>
-//         <button onClick={this.handleClick}>{books.length < 3 ? '책추가' : '책정지' }</button>
-//         <ul>
-//           {books.map((book: IBook) => (
-//             <li>
-//               <input type="checkbox" class="toggle" checked={book.completed} />
-//               {book.content}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     )
-//   }
-// }
-
-// Dj.render(<App />, document.querySelector('#root'));
